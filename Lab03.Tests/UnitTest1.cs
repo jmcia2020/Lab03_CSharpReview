22956cbd33946dabfbc04152ffa346bc9e74b596
jmcia2020 <jeanmmcmahon@gmail.com>
2021-03-25 00:30:58 -0500

    first test failed

--- Lab03.Tests/UnitTest1.cs
+++ Lab03.Tests/UnitTest1.cs
@@ -6,15 +6,18 @@ namespace Lab03.Tests
     public class Challenge1Tests1
     {
         [Fact]
-        public void Challenge1()
+        public void UserArray_returns_an_array_of_3_integers()
         {
-            Program
 
             //Arrange
+            int[] userArray = new int[3];
 
             //Act
+            int[] result = [userArray];
 
             //Assert
+            Assert.Equal(3, result.Length);
+
         }
     }
 }
