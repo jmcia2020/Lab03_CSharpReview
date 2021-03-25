55feb588e3da8256e88902e93fb9e3e6fa77fd0f
jmcia2020 <jeanmmcmahon@gmail.com>
2021-03-24 23:38:46 -0500

    first attempt first test

---
+++ Lab03.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Lab03.Tests
+{
+    public class Challenge1Tests1
+    {
+        [Fact]
+        public void Challenge1()
+        {
+            Program
+
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Lab03_CSharpReview
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
